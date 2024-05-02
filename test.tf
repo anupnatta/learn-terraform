35c183e5fab06475f86ce479005a578567ae6e31
anupn <anup.natta@gmail.com>
2024-05-02 14:48:52 +0530

    terraform test1

---
+++ test.tf
@@ -0,0 +1,8 @@
+ resource "aws_instance" "ec2" {
+   ami = "ami-0f3c7d07486cad139"
+   instance_type = "t3.micro"
+   vpc_security_group_ids = [sg-043db02ec7e17c9ae]
+   tags = {
+     Name = test""
+   }
+ }
