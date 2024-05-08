f345f46e73fd60d16d9edea375cbcb7de742ece5
anupn <anup.natta@gmail.com>
2024-05-08 15:53:59 +0530

    terraform roboshop s2/2

--- statefile/main.tf
+++ statefile/main.tf
@@ -6,7 +6,7 @@ terraform {
   }
 }
 
-resource "aws_spot_instance_request" "ec2" {
+resource "aws_instance" "ec2" {
   ami = "ami-0f3c7d07486cad139"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-043db02ec7e17c9ae"]
