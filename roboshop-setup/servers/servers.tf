510138a6ad7d8fbf990b47e5d28b063f9161c85f
anupn <anup.natta@gmail.com>
2024-05-08 11:46:37 +0530

    terraform frontend2

--- roboshop-setup/servers/servers.tf
+++ roboshop-setup/servers/servers.tf
@@ -12,6 +12,6 @@ resource "aws_route53_record" "frontend" {
   name    = "frontend-dev.devop69.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.ec2.private_ip]
 }
 ###
