c28f0480e8231a7a9502a87201495a6fd68bef5e
anupn <anup.natta@gmail.com>
2024-05-08 10:26:18 +0530

    terraform vars

--- variables/var.tf
+++ variables/var.tf
@@ -13,3 +13,26 @@ variable "sample1" {
 output "sample1" {
   value = var.sample1
 }
+
+variable "input" {}
+
+variable "sample4" {
+  default = {
+           "Hello",
+    1000,
+    true,
+    "Anup"
+  }
+}
+variable "sample5" {
+  default = {
+    string = "Hello Anup",
+    numeber = 100,
+    boolean = true
+  }
+}
+
+output "types" {
+  value = "Variable sample1 - ${var.sample1}, First value in list - ${var.sample4[0]}, Boolean Value of Map = ${var.sample5["boolean"]}"
+  }
+}
