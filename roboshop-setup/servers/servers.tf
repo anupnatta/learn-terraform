2d20d455d373c94f2436ecb1cb73238983a7d5fa
anupn <anup.natta@gmail.com>
2024-05-08 11:39:57 +0530

    terraform frontend1

--- roboshop-setup/servers/servers.tf
+++ roboshop-setup/servers/servers.tf
@@ -1,8 +1,17 @@
-resource "aws_instance" "frontend" {
-  ami           = ami-0f3c7d07486cad139
-  instance_type = "t3.micro"
-
-  tags = {
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-043db02ec7e17c9ae"]
+  tags                   = {
     Name = "frontend"
   }
 }
+
+resource "aws_route53_record" "frontend" {
+  zone_id = Z024789331TA1ZYTI6SDI
+  name    = "frontend-dev.devop69.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+###
--- sample/test.tf
+++ sample/test.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "ec2" {
   ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-043db02ec7e17c9ae"]
